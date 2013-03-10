9bb705ee136337adcafca6bed26afcc390b380f9
Daniel Hernández <daniel@degu.cl>
2013-03-09 21:24:32 -0300

    Fixing: Adding interfaces.py

---
+++ src/degu/forum/interfaces.py
@@ -0,0 +1,8 @@
+from zope.interface import Interface
+from zope import schema
+
+from degu.forum import DeguForumMessageFactory as _
+
+class IDeguSettings(Interface):
+    """Describes registry records
+    """
